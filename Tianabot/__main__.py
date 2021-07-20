1c263968b28de8994fdd65657bb78571c3dd7201
sohailkhan-project <78003723+sohailkhan-project@users.noreply.github.com>
2021-07-20 13:31:28 +0530

    Update __main__.py

--- Tianabot/__main__.py
+++ Tianabot/__main__.py
@@ -82,10 +82,10 @@ def get_readable_time(seconds: int) -> str:
 
 PM_START_TEXT = f"""Hello, My Name is {BOT_NAME}
 
-Hey , I am create to help my Hubby @SohailKhan_Anime,
+Hey , I am create to help my Hubby's Group @SohailKhan_Anime,
 Made specially for Managing Group.
 
-This Probot was Created by [ℙℝ𝕀ℕℂ𝔼](https://t.me/devilsdad_prince)
+
 Click /help or use button below to find out more about how to use me to my full potential."""
 
 buttons = [
